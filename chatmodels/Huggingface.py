2fedfd058e5034037e398b93f368fe546abb8b5e
Shivam Dwivedi <shivamdwivedicse20919@gmail.com>
2026-07-07 10:12:08 +0530

    Added practiced huggingface models

---
+++ chatmodels/Huggingface.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+
+llm = HuggingFaceEndpoint(
+    repo_id="deepseek-ai/DeepSeek-R1-Distill-Qwen-7B",
+    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_API_TOKEN")
+)
+
+model = ChatHuggingFace(llm=llm)
+
+response = model.invoke("Who are you")
+print(response.content)
